7e9f2efc1719ae7a5dd12f065835ea8cafeb4a0f
boyce <hepeilin1987@qq.com>
2014-05-19 23:58:27 +0800

    给图片加水印
    给图片加水印

--- Method/Tools.h
+++ Method/Tools.h
@@ -15,4 +15,27 @@
 + (BOOL)isValidEmail:(NSString*)emailAdr;
 //获取设备类型
 + (NSString*)getDeviceModel;
+
+/**
+ 加文字随意
+ @param img 需要加文字的图片
+ @param text1 文字描述
+ @returns 加好文字的图片
+ */
++ (UIImage *)addText:(UIImage *)img text:(NSString *)text1;
+
+/**
+ 加图片水印
+ @param img 需要加logo图片的图片
+ @param logo logo图片
+ @returns 加好logo的图片
+ */
++ (UIImage *)addImageLogo:(UIImage *)img text:(UIImage *)logo;
+/**
+ 加半透明水印
+ @param useImage 需要加水印的图片
+ @param addImage1 水印
+ @returns 加好水印的图片
+ */
++ (UIImage *)addImage:(UIImage *)useImage addMsakImage:(UIImage *)maskImage;
 @end
--- Method/Tools.m
+++ Method/Tools.m
@@ -71,7 +71,97 @@
     
     return model;
     
-    
 }
+
+
+/**
+ 加文字随意
+ @param img 需要加文字的图片
+ @param text1 文字描述
+ @returns 加好文字的图片
+ */
++ (UIImage *)addText:(UIImage *)img text:(NSString *)text1
+{
+    
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_6_1
+    int bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedLast;
+#else
+    int bitmapInfo = kCGImageAlphaPremultipliedLast;
+#endif
+    
+    int w = 300;
+    int h = 140;
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //create a graphic context with CGBitmapContextCreate
+    CGContextRef context = CGBitmapContextCreate(NULL, w, h, 8, 4 * w, colorSpace, bitmapInfo);
+    CGContextDrawImage(context, CGRectMake(0, 0, w, h), img.CGImage);
+    CGContextSetRGBFillColor(context, 0.0, 1.0, 1.0, 1);
+    char* text = (char *)[text1 cStringUsingEncoding:NSASCIIStringEncoding];
+    CGContextSelectFont(context, "Georgia", 15, kCGEncodingMacRoman);
+    CGContextSetTextDrawingMode(context, kCGTextFill);
+    CGContextSetRGBFillColor(context, 255, 255, 255, 0.5);
+    
+    //位置调整
+    CGContextShowTextAtPoint(context, w/2-strlen(text)*4.5 , h - 135, text, strlen(text));
+    
+    //Create image ref from the context
+    CGImageRef imageMasked = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return [UIImage imageWithCGImage:imageMasked];
+}
+
+/**
+ 加图片水印
+ @param img 需要加logo图片的图片
+ @param logo logo图片
+ @returns 加好logo的图片
+ */
++ (UIImage *)addImageLogo:(UIImage *)img text:(UIImage *)logo
+{
+    //get image width and height
+    
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_6_1
+    int bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedLast;
+#else
+    int bitmapInfo = kCGImageAlphaPremultipliedLast;
+#endif
+    
+    int w = img.size.width;
+    int h = img.size.height;
+    int logoWidth = logo.size.width;
+    int logoHeight = logo.size.height;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //create a graphic context with CGBitmapContextCreate
+    CGContextRef context = CGBitmapContextCreate(NULL, w, h, 8, 4 * w, colorSpace, bitmapInfo);
+    CGContextDrawImage(context, CGRectMake(0, 0, w, h), img.CGImage);
+    CGContextDrawImage(context, CGRectMake(w-logoWidth, 0, logoWidth, logoHeight), [logo CGImage]);
+    CGImageRef imageMasked = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return [UIImage imageWithCGImage:imageMasked];
+    //  CGContextDrawImage(contextRef, CGRectMake(100, 50, 200, 80), [smallImg CGImage]);
+}
+
+/**
+ 加半透明水印
+ @param useImage 需要加水印的图片
+ @param addImage1 水印
+ @returns 加好水印的图片
+ */
++ (UIImage *)addImage:(UIImage *)useImage addMsakImage:(UIImage *)maskImage
+{
+    UIGraphicsBeginImageContext(useImage.size);
+    [useImage drawInRect:CGRectMake(0, 0, useImage.size.width, useImage.size.height)];
+    //    [addImage1 drawInRect:CGRectMake(0, useImage.size.height-addImage1.size.height, addImage1.size.width, addImage1.size.height)];
+    
+    //四个参数为水印图片的位置
+    [maskImage drawInRect:CGRectMake(useImage.size.width - 110, useImage.size.height-25, 100, 25)];
+    UIImage *resultingImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resultingImage;
+}
+
 
 @end
