d3242267fbf4182c57534943c09ef53cdb4f66f6
boyce <hepeilin1987@qq.com>
2014-05-19 20:00:31 +0800

    获取iOS序列号
    rt

---
+++ BHLibraries/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BHLibraries
+//
+//  Created by hepeilin on 14-5-19.
+//  Copyright (c) 2014年 boyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Method/Tools.h
@@ -0,0 +1,15 @@
+//
+//  Tools.h
+//  BHLibraries
+//
+//  Created by hepeilin on 14-5-19.
+//  Copyright (c) 2014年 boyce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tools : NSObject
+//获取序列号
+- (NSString*)getDeviceSerialNumber;
+
+@end
