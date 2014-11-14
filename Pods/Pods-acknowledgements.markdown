79cf2eb6a27623ba49afb23b39af10f6e5a5a8cd
boyce <hepeilin1987@qq.com>
2014-11-14 15:43:05 +0800

    pod update
    pod update

--- Pods/Pods-acknowledgements.markdown
+++ Pods/Pods-acknowledgements.markdown
@@ -599,7 +599,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## SAMTextField
 
-Copyright (c) 2010-2013 Sam Soffes, http://soff.es
+Copyright (c) 2010-2014 Sam Soffes, http://soff.es
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -9,80 +9,80 @@
 // AFNetworking
 #define COCOAPODS_POD_AVAILABLE_AFNetworking
 #define COCOAPODS_VERSION_MAJOR_AFNetworking 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
 // AFNetworking/NSURLConnection
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 1
 
 // AFNetworking/NSURLSession
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 1
 
 // AFNetworking/Reachability
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 1
 
 // AFNetworking/Security
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 1
 
 // AFNetworking/Serialization
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 1
 
 // AFNetworking/UIKit
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 2
-#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 4
+#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 4
+#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
 
 // BlocksKit
 #define COCOAPODS_POD_AVAILABLE_BlocksKit
 #define COCOAPODS_VERSION_MAJOR_BlocksKit 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit 5
 
 // BlocksKit/All
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_All
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_All 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_All 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_All 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_All 5
 
 // BlocksKit/Core
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_Core
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_Core 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_Core 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_Core 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_Core 5
 
 // BlocksKit/DynamicDelegate
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_DynamicDelegate
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_DynamicDelegate 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_DynamicDelegate 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_DynamicDelegate 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_DynamicDelegate 5
 
 // BlocksKit/MessageUI
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_MessageUI
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_MessageUI 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_MessageUI 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_MessageUI 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_MessageUI 5
 
 // BlocksKit/UIKit
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_UIKit
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_UIKit 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_UIKit 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_UIKit 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_UIKit 5
 
 // CocoaAsyncSocket
 #define COCOAPODS_POD_AVAILABLE_CocoaAsyncSocket
@@ -100,7 +100,7 @@
 // MBProgressHUD
 #define COCOAPODS_POD_AVAILABLE_MBProgressHUD
 #define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
-#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
 
 // MagicalRecord
@@ -269,7 +269,7 @@
 #define COCOAPODS_POD_AVAILABLE_SAMCategories
 #define COCOAPODS_VERSION_MAJOR_SAMCategories 0
 #define COCOAPODS_VERSION_MINOR_SAMCategories 5
-#define COCOAPODS_VERSION_PATCH_SAMCategories 1
+#define COCOAPODS_VERSION_PATCH_SAMCategories 2
 
 // SAMCircleProgressView
 #define COCOAPODS_POD_AVAILABLE_SAMCircleProgressView
@@ -311,7 +311,7 @@
 #define COCOAPODS_POD_AVAILABLE_SAMTextField
 #define COCOAPODS_VERSION_MAJOR_SAMTextField 0
 #define COCOAPODS_VERSION_MINOR_SAMTextField 1
-#define COCOAPODS_VERSION_PATCH_SAMTextField 1
+#define COCOAPODS_VERSION_PATCH_SAMTextField 2
 
 // SAMTextView
 #define COCOAPODS_POD_AVAILABLE_SAMTextView
@@ -341,7 +341,7 @@
 #define COCOAPODS_POD_AVAILABLE_TMCache
 #define COCOAPODS_VERSION_MAJOR_TMCache 1
 #define COCOAPODS_VERSION_MINOR_TMCache 2
-#define COCOAPODS_VERSION_PATCH_TMCache 0
+#define COCOAPODS_VERSION_PATCH_TMCache 2
 
 // ZipArchive
 #define COCOAPODS_POD_AVAILABLE_ZipArchive
@@ -353,5 +353,5 @@
 #define COCOAPODS_POD_AVAILABLE_pop
 #define COCOAPODS_VERSION_MAJOR_pop 1
 #define COCOAPODS_VERSION_MINOR_pop 0
-#define COCOAPODS_VERSION_PATCH_pop 5
+#define COCOAPODS_VERSION_PATCH_pop 7
 
